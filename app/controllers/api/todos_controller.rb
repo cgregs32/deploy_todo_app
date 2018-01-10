0036e8ffa82686f265b7ab0d667facf61a2873d4
cgregs32 <cgregs32@gmail.com>
2018-01-10 12:00:04 -0700

    basic todo

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,19 @@
+class Api::TodosController < ApplicationController
+  def index
+    render json: Todo.order(created_at: :desc)
+  end
+
+  def create
+    todo = Todo.create(name: params[:name])
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    todo.update(complete: !todo.complete)
+    render json: todo
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+  end
+end
